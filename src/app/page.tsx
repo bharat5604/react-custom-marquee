c5e6cced6b14c98763b8b5f86e6710bbea4e747d
bharat <bhushansingh696@gmail.com>
2024-02-13 15:09:11 +0530

    adding hooks

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,78 +2,76 @@
 import Image from "next/image";
 import $ from "jquery";
 import { useEffect } from "react";
+import useNewsTicker from "@/hooks/useNewsTicker";
 
 export default function Home() {
   useEffect(() => {
-    $(document).ready(function (e) {
-      var newsNum = $(".newsTicker li").length;
-
-      var totalNews_H = 0;
-      for (var i = 0; i < newsNum; i++) {
-        var news_H: any = $(".newsTicker li").eq(i).outerHeight(true);
-        totalNews_H = totalNews_H + news_H;
-      }
-
-      $(".newsTicker").css("height", totalNews_H + "px");
-
-      var speed = 1,
-        animAmount,
-        animInterval: any;
-
-      animateNews();
-
-      function animateNews() {
-        $(".newsTicker li")
-          .eq(0)
-          .animate(
-            {
-              marginTop: "-=" + speed + "px",
-            },
-            1,
-            function () {
-              animAmount = speed;
-              var animNews_H: any = $(".newsTicker li").eq(0).outerHeight(true);
-
-              if (animAmount >= animNews_H) {
-                $(this).parent().append($(this));
-                $(this).removeAttr("style");
-              }
-
-              animInterval = setTimeout(function () {
-                animateNews();
-              }, 50);
-            }
-          );
-      }
-
-      $(".newsTicker").hover(
-        function () {
-          clearTimeout(animInterval);
-          $(".newsTicker li").eq(0).stop();
-        },
-        function () {
-          animateNews();
-        }
-      );
-    });
+    // $(document).ready(function (e) {
+    //   let newsNum = $(".newsTicker li").length;
+    //   let totalNews_H = 0;
+    //   for (let i = 0; i < newsNum; i++) {
+    //     let news_H: any = $(".newsTicker li").eq(i).outerHeight(true);
+    //     totalNews_H = totalNews_H + news_H;
+    //   }
+    //   $(".newsTicker").css("height", totalNews_H + "px");
+    //   let speed = 1,
+    //     animAmount,
+    //     animInterval: any;
+    //   animateNews();
+    //   function animateNews() {
+    //     $(".newsTicker li")
+    //       .eq(0)
+    //       .animate(
+    //         {
+    //           marginTop: "-=" + speed + "px",
+    //         },
+    //         1,
+    //         function () {
+    //           animAmount = speed;
+    //           let animNews_H: any = $(".newsTicker li").eq(0).outerHeight(true);
+    //           if (animAmount >= animNews_H) {
+    //             $(this).parent().append($(this));
+    //             $(this).removeAttr("style");
+    //           }
+    //           animInterval = setTimeout(function () {
+    //             animateNews();
+    //           }, 50);
+    //         }
+    //       );
+    //   }
+    //   $(".newsTicker").hover(
+    //     function () {
+    //       clearTimeout(animInterval);
+    //       $(".newsTicker li").eq(0).stop();
+    //     },
+    //     function () {
+    //       animateNews();
+    //     }
+    //   );
+    // });
   }, []);
+
+  useNewsTicker({ speed: 1, interval: 60, className: "newsTicker" });
+  useNewsTicker({ speed: 1, interval: 60, className: "newsTicker1" });
   return (
-    <main className="">
+    <main className="flex">
       <div className="wrap">
         <div className="newsTickerCon">
           <ul className="newsTicker">
             <li>
               <img src="https://us-dev-media.appskeeper.in/assets/_default_upload_bucket/3425175_59877.svg" />
-              <div className="headline">
-                PALTOP invites you to take a closer look
-              </div>
+              <div className="p-2">
+                <div className="headline">
+                  PALTOP invites you to take a closer look
+                </div>
 
-              <p>
-                company and learn about the technology and considerations behind
-                our outstanding products.
-                <br />
-                <a href="">read more</a>
-              </p>
+                <p>
+                  company and learn about the technology and considerations
+                  behind our outstanding products.
+                  <br />
+                  <a href="">read more</a>
+                </p>
+              </div>
             </li>
             <li>
               <img src="https://us-dev-media.appskeeper.in/assets/_default_upload_bucket/3425175_59877.svg" />
@@ -92,6 +90,144 @@ export default function Home() {
               <img src="https://us-dev-media.appskeeper.in/assets/_default_upload_bucket/3425175_59877.svg" />
               <div className="headline">
                 Lecture &amp; Hands-on Course, New York 2018
+              </div>
+
+              <p>
+                company and learn about the technology and considerations behind
+                our outstanding products.
+                <br />
+                <a href="">read more</a>
+              </p>
+            </li>
+            <li>
+              <img src="https://us-dev-media.appskeeper.in/assets/_default_upload_bucket/3425175_59877.svg" />
+              <div className="headline">
+                Lecture and Live Surgery by Dr. Michael Klein
+              </div>
+
+              <p>
+                company and learn about the technology and considerations behind
+                our outstanding products.
+                <br />
+                <a href="">read more</a>
+              </p>
+            </li>
+            <li>
+              <img src="https://us-dev-media.appskeeper.in/assets/_default_upload_bucket/3425175_59877.svg" />
+              <div className="headline">
+                Lecture and Live Surgery by Dr. Michael Klein
+              </div>
+
+              <p>
+                company and learn about the technology and considerations behind
+                our outstanding products.
+                <br />
+                <a href="">read more</a>
+              </p>
+            </li>
+            <li>
+              <img src="https://us-dev-media.appskeeper.in/assets/_default_upload_bucket/3425175_59877.svg" />
+              <div className="headline">
+                Lecture and Live Surgery by Dr. Michael Klein
+              </div>
+
+              <p>
+                company and learn about the technology and considerations behind
+                our outstanding products.
+                <br />
+                <a href="">read more</a>
+              </p>
+            </li>
+            <li>
+              <img src="https://us-dev-media.appskeeper.in/assets/_default_upload_bucket/3425175_59877.svg" />
+              <div className="headline">
+                Lecture and Live Surgery by Dr. Michael Klein
+              </div>
+
+              <p>
+                company and learn about the technology and considerations behind
+                our outstanding products.
+                <br />
+                <a href="">read more</a>
+              </p>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="wrap">
+        <div className="newsTickerCon">
+          <ul className="newsTicker1">
+            <li>
+              <img src="https://us-dev-media.appskeeper.in/assets/_default_upload_bucket/3425175_59877.svg" />
+              <div className="p-2">
+                <div className="headline">
+                  PALTOP invites you to take a closer look
+                </div>
+
+                <p>
+                  company and learn about the technology and considerations
+                  behind our outstanding products.
+                  <br />
+                  <a href="">read more</a>
+                </p>
+              </div>
+            </li>
+            <li>
+              <img src="https://us-dev-media.appskeeper.in/assets/_default_upload_bucket/3425175_59877.svg" />
+              <div className="headline">
+                Practical Course in Basic or Advanced Implantology
+              </div>
+
+              <p>
+                company and learn about the technology and considerations behind
+                our outstanding products.
+                <br />
+                <a href="">read more</a>
+              </p>
+            </li>
+            <li>
+              <img src="https://us-dev-media.appskeeper.in/assets/_default_upload_bucket/3425175_59877.svg" />
+              <div className="headline">
+                Lecture &amp; Hands-on Course, New York 2018
+              </div>
+
+              <p>
+                company and learn about the technology and considerations behind
+                our outstanding products.
+                <br />
+                <a href="">read more</a>
+              </p>
+            </li>
+            <li>
+              <img src="https://us-dev-media.appskeeper.in/assets/_default_upload_bucket/3425175_59877.svg" />
+              <div className="headline">
+                Lecture and Live Surgery by Dr. Michael Klein
+              </div>
+
+              <p>
+                company and learn about the technology and considerations behind
+                our outstanding products.
+                <br />
+                <a href="">read more</a>
+              </p>
+            </li>
+            <li>
+              <img src="https://us-dev-media.appskeeper.in/assets/_default_upload_bucket/3425175_59877.svg" />
+              <div className="headline">
+                Lecture and Live Surgery by Dr. Michael Klein
+              </div>
+
+              <p>
+                company and learn about the technology and considerations behind
+                our outstanding products.
+                <br />
+                <a href="">read more</a>
+              </p>
+            </li>
+            <li>
+              <img src="https://us-dev-media.appskeeper.in/assets/_default_upload_bucket/3425175_59877.svg" />
+              <div className="headline">
+                Lecture and Live Surgery by Dr. Michael Klein
               </div>
 
               <p>
---
+++ src/hooks/useNewsTicker.tsx
@@ -0,0 +1,72 @@
+import { useEffect } from "react";
+import $ from "jquery";
+
+interface NewsTickerOptions {
+  speed: number;
+  interval: number;
+  className: string;
+}
+
+const useNewsTicker = (options: NewsTickerOptions) => {
+  useEffect(() => {
+    const { speed, interval, className } = options;
+
+    let newsNum = $(`.${className} li`).length;
+    let totalNews_H = 0;
+
+    for (let i = 0; i < newsNum; i++) {
+      let news_H: number | undefined = $(`.${className} li`)
+        .eq(i)
+        .outerHeight(true);
+      totalNews_H = totalNews_H + news_H!;
+    }
+
+    $(`.${className}`).css("height", totalNews_H + "px");
+
+    let animInterval: number;
+
+    animateNews();
+
+    function animateNews() {
+      $(`.${className} li`)
+        .eq(0)
+        .animate(
+          {
+            marginTop: `-=${speed}px`,
+          },
+          1,
+          function () {
+            let animAmount = speed;
+            let animNews_H: number | undefined = $(`.${className} li`)
+              .eq(0)
+              .outerHeight(true);
+
+            if (animAmount >= animNews_H!) {
+              $(this).parent().append($(this));
+              $(this).removeAttr("style");
+            }
+
+            animInterval = window.setTimeout(function () {
+              animateNews();
+            }, interval);
+          }
+        );
+    }
+
+    $(`.${className}`).hover(
+      function () {
+        clearTimeout(animInterval);
+        $(`.${className} li`).eq(0).stop();
+      },
+      function () {
+        animateNews();
+      }
+    );
+
+    return () => {
+      clearTimeout(animInterval);
+    };
+  }, [options.speed, options.interval, options.className, options]);
+};
+
+export default useNewsTicker;
